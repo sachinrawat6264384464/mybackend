858aa928628975b4bbe0f52bdf0582387552543e
website <sacinrawat6264384464@gmail.com>
2025-09-22 22:16:06 +0530

    hh

--- mybackend/urls.py
+++ mybackend/urls.py
@@ -15,9 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Backend is running successfully 🚀")
 
 urlpatterns = [
+    path("", home),  # root URL
     path("admin/", admin.site.urls),
-    path("api/", include("Api.urls")),
+    path("api/", include("Api.urls")),  # tumhari app ka urls.py
 ]
 
